f62bcdf89efe08657a65b38847ee0339bd9841c4
coelslager <157177563+coelslager@users.noreply.github.com>
2025-04-15 19:06:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //event listener - trigger when the DOM is loaded (when visiting the web page)
 addEventListener("DOMContentLoaded", async function(){
-    const response = await fetch('http://localhost:3000/api/songs');
+    const response = await fetch('https://common-full-tractor.glitch.me/api/songs');
     const songs = await response.json();
     console.log(songs);
 
